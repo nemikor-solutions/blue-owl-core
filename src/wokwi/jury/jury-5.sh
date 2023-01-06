c60d1d73da50e5685e16228c07874052e088af59
Jean-François Lamy <jf@jflamy.dev>
2023-01-06 14:14:14 -0500

    moved wokwi up

---
+++ src/wokwi/jury/jury-5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -
+export BLUE_OWL_MQTT_PASSWORD=test
+export BLUE_OWL_MQTT_URL=mqtt://127.0.0.1:1883
+export BLUE_OWL_MQTT_USERNAME=test
+export BLUE_OWL_PLATFORM=A
+export BLUE_OWL_SERIAL_PORT=COM6
+
+npm run ts ./src/wokwi/referee/referee.ts
