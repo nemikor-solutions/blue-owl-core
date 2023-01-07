52555036cb16d89f28cd521ed4f2d6eeb03f23e7
Jean-François Lamy <jf@jflamy.dev>
2023-01-06 20:34:15 -0500

    force use of locally unzipped node

--- src/wokwi/referee/referee-single.sh
+++ src/wokwi/referee/referee-single.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -
+
+# force the use of our copy of node
+SCRIPTDIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
+PATH="$SCRIPTDIR/../../../node;$PATH"
+
 export BLUE_OWL_MQTT_PASSWORD=test
 export BLUE_OWL_MQTT_URL=mqtt://127.0.0.1:1883
 export BLUE_OWL_MQTT_USERNAME=test
--- src/wokwi/referee/referee.sh
+++ src/wokwi/referee/referee.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -
+
+# force the use of our copy of node
+SCRIPTDIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
+PATH="$SCRIPTDIR/../../../node;$PATH"
+
 export BLUE_OWL_MQTT_PASSWORD=test
 export BLUE_OWL_MQTT_URL=mqtt://127.0.0.1:1883
 export BLUE_OWL_MQTT_USERNAME=test
 export BLUE_OWL_PLATFORM=A
 export BLUE_OWL_SERIAL_PORT=COM6
 
+which npm
 npm run ts ./src/wokwi/referee/referee.ts
