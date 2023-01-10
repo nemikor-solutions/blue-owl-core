0913abee2f941bb7f38f3f8be31220d216a9e28a
Scott González <scott.gonzalez@gmail.com>
2023-01-09 20:05:51 -0500

    Add `DigitalRgb` component
    Johnny-Five doesn't support RGB LEDs on non-PWM pins since almost all
    colors require `analogWrite()`. However, we only care about colors that
    can be achieved with digital writes (green, red, and white). This adds
    a new component that explicitly provides support for those three colors
    using any digital pins.

--- src/lib/model/jury-member/rgb-led.ts
+++ src/lib/model/jury-member/rgb-led.ts
@@ -1,37 +1,37 @@
 import type JuryMember from 'lib/model/jury-member';
 import type {
-    Board,
-} from 'johnny-five';
+    DigitalRgbOptions,
+} from 'lib/johnny-five/digital-rgb';
 
-import {
-    Led,
-} from 'johnny-five';
+import DigitalRgb from 'lib/johnny-five/digital-rgb';
 
 export interface JuryRefereeLedsOptions {
-    anode?: Led.RGBOption['isAnode'];
-    board?: Board;
-    pins: Led.RGBOption['pins'];
+    anode?: DigitalRgbOptions['anode'];
+    board?: DigitalRgbOptions['board'];
+    pins: DigitalRgbOptions['pins'];
 }
 
 export default (options: JuryRefereeLedsOptions) => {
-    const led = new Led.RGB({
+    const led = new DigitalRgb({
+        anode: options.anode,
         board: options.board,
         pins: options.pins,
-        isAnode: options.anode,
     });
 
     return (juryMember: JuryMember) => {
         juryMember.on('decision', () => {
-            led.color('#00ff00')
-            led.on();
+            console.log('decision', juryMember.number);
+            led.green();
         });
 
         juryMember.on('reset', () => {
+            console.log('reset', juryMember.number);
             led.off();
         });
 
         juryMember.on('reveal', ({ decision }) => {
-            led.color(decision === 'good' ? '#ffffff' : '#ff0000');
+            console.log('reveal', decision, juryMember.number);
+            led[decision === 'good' ? 'white' : 'red']();
         });
     };
 }
