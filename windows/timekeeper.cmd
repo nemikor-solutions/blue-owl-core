224a037b155025b5bc1f23b3a4bfcb78719fd5e5
Jean-François Lamy <jf@jflamy.dev>
2023-01-19 09:09:42 -0500

    config changes

--- windows/timekeeper.cmd
+++ windows/timekeeper.cmd
@@ -1,6 +1,6 @@
 call %~dp0/config.cmd
 
 rem remove rem from the following line to set the port explicitly
-rem set BLUE_OWL_SERIAL_PORT=COM6
+set BLUE_OWL_SERIAL_PORT=COM10
 
 npm run ts build-it-yourself/scripts/timekeeper.ts
