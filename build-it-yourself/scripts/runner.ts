a9d20f99f6c1a7f51e29a78f1798153e09e43f3b
Jean-François Lamy <jf@jflamy.dev>
2023-01-22 09:32:45 -0500

    reverted to upstream, but relative paths

---
+++ build-it-yourself/scripts/runner.ts
@@ -0,0 +1,59 @@
+import type {
+    Board,
+} from 'johnny-five';
+import type {
+    Config,
+} from './config';
+
+import connectBoard from '../lib/board';
+import Owlcms from '../lib/owlcms';
+import parseConfig from '../scripts/config';
+
+export type Initializer = (options: InitializerOptions) => void;
+
+export interface InitializerOptions {
+    board: Board;
+    config: Config;
+    owlcms: Owlcms;
+    platform: Config['platform'];
+}
+
+export default async (initializer: Initializer, config?: Config) => {
+    config ||= parseConfig();
+
+    const owlcms = new Owlcms({
+        mqttPassword: config.mqttPassword,
+        mqttUrl: config.mqttUrl,
+        mqttUsername: config.mqttUsername,
+    });
+
+    try {
+        await owlcms.connect();
+
+        let board;
+        if (config.serialPort) {
+            board = await connectBoard({
+                port: config.serialPort,
+            });
+        } else {
+            board = await connectBoard({});
+        }
+
+        initializer({
+            board,
+            config,
+            owlcms,
+            platform: config.platform,
+        });
+    } catch (error) {
+        console.error(error);
+        process.exitCode = 1;
+
+        owlcms.disconnect();
+
+        // If the process does not exit cleanly within five seconds, force close
+        setTimeout(() => {
+            process.exit(2);
+        }, 5000);
+    }
+}
