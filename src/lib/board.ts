8c1e8d582407bc682f61812246b50af4e3463860
Jean-François Lamy <jf@jflamy.dev>
2023-01-22 12:08:49 -0500

    workaround for j5 failing to autodetect

--- src/lib/board.ts
+++ src/lib/board.ts
@@ -7,8 +7,24 @@ import {
 } from 'johnny-five';
 
 export default (options: BoardOption = {}) => {
-    const board = new Board({ repl: false, ...options });
+    let board: Board;
+    try {
+        board = new Board({ repl: false, ...options });
+    } catch (error) {
+        console.log()
+        if (! options.port) {
+            console.error("Error: Could not autodetect the device. Please provide the communication port name.")
+        } else {
+            console.error("Error: "+error)
+        }
+        console.log()
+        // give time to read messages when running in a command window
+        setTimeout(() => {process.exit();}, 10000)
 
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        return new Promise<Board>((_resolve, _reject) => {return;})
+    }
+    
     return new Promise<Board>((resolve, reject) => {
         board.on('ready', () => {
             resolve(board);
